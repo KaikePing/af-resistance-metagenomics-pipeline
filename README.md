59250442577f295dffc6d1b1c23de02b5e48dd56
Kaike Ping <pkk@vt.edu>
2024-05-07 16:10:28 -0400

    chore: Add sra_downloader.py script for downloading and extracting fastq files from SRA

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # af-resistance-metagenomics-pipeline
+
 A computational pipeline for identifying antifungal-resistant Aspergillus fumigatus strains in metagenomic data.
+
+## Requirement
+
+- sra-tools
+- Kraken2
+
+## Data Retrieval
+
+### sra_downloader.py
+
+This script is used to download and extract `fastq` files from the Sequence Read Archive (SRA). It can take either a single SRA ID or a file containing multiple SRA IDs (one per line) as input.
+
+#### Usage
+
+```bash
+python sra_downloader.py <input>
+```
+
+Where `<input>` is either a single SRA ID or a file containing multiple SRA IDs (one per line).
+
+#### Example
+
+```bash
+python sra_downloader.py SRR123456
+```
+
+This command will download the .sra file for SRA ID SRR123456 and extract the `fastq` files.
+
+```bash
+python sra_downloader.py sra_ids.txt
+```
+
+This command will read the file `sra_ids.txt` (each line containing a single SRA ID) and download and extract the `fastq` files for each SRA ID.
---
+++ sra_downloader.py
@@ -0,0 +1,34 @@
+from tqdm import tqdm
+import os, argparse
+
+# Read the metadata file
+def read_sra_numbers(input):
+    if os.path.isfile(input):
+        with open(input) as f:
+            return f.read().splitlines()
+    else:
+        return [input]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download and extract fastq files from SRA")
+    parser.add_argument('input', help='SRA number or file containing SRA numbers (one per line)')
+
+    args = parser.parse_args()
+    
+    sra_numbers = read_sra_numbers(args.input)
+
+    # this will download the .sra files to data/ (will create directory if not present)
+    for sra_id in tqdm(sra_numbers):
+        print ("Currently downloading: " + sra_id)
+        prefetch_command = 'prefetch -O ./data ' + sra_id
+        print ("The command used was: " + prefetch_command)
+        os.system(prefetch_command)
+
+    # this will extract the .sra files from above into a folder named 'fastq'
+    for sra_id in tqdm(sra_numbers):
+        print ("Generating fastq for: " + sra_id)
+        fastq_dump_command = 'fasterq-dump --outdir ./fastq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-3 --clip ./data/' + sra_id + '/' + sra_id + '.sra'
+        print ("The command used was: " + fastq_dump_command)
+        os.system(fastq_dump_command)
+
+    print ("All done!")
